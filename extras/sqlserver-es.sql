b9459de2f26758448f9b79f67539e61d91c838f6
jnthn <jnthn@jnthn.net>
2014-03-28 17:47:21 +0100

    Add SQL script for SQL-based event store.
    So folks don't have to reverse-engineer it from the code.

---
+++ extras/sqlserver-es.sql
@@ -0,0 +1,62 @@
+USE [SomeDatabase]
+GO
+
+/****** Object:  Table [dbo].[Events]    Script Date: 03/28/2014 17:37:55 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Events](
+	[AggregateId] [uniqueidentifier] NOT NULL,
+	[SequenceNumber] [int] NOT NULL,
+	[Type] [varchar](1000) NOT NULL,
+	[Body] [text] NOT NULL,
+	[Timestamp] [datetime] NOT NULL,
+	[PollOrder] [int] IDENTITY(1,1) NOT NULL,
+ CONSTRAINT [PK_Events] PRIMARY KEY NONCLUSTERED 
+(
+	[AggregateId] ASC,
+	[SequenceNumber] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+/****** Object:  Index [IX_Events]    Script Date: 03/28/2014 17:46:09 ******/
+CREATE CLUSTERED INDEX [IX_Events] ON [dbo].[Events] 
+(
+	[PollOrder] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+GO
+
+/****** Object:  Table [dbo].[Aggregates]    Script Date: 03/28/2014 17:41:48 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Aggregates](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Type] [varchar](1000) NOT NULL,
+ CONSTRAINT [PK_Aggregates] PRIMARY KEY NONCLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
